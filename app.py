07e2b99c60c97f4ff861343bba1d3bd91cdc07ed
mohamed anas <mmohamedanas235@gmail.com>
2026-01-19 00:40:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,6 +61,4 @@ def predict():
     return jsonify(response)
 
 print("Prediction route '/predict' defined successfully.")
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+
