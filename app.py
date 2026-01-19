9696fec79c8bb637cd874c1be1f8fdad1f27a944
mohamed anas <mmohamedanas235@gmail.com>
2026-01-19 17:07:22 +0530

    Enable CORS for Flask application

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ import pandas as pd
 import os
 import joblib
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 
 # Load the StandardScaler object
 scaler = joblib.load('scaler.joblib')
